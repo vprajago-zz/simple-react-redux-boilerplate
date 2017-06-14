7eab406bc5dfab000b0adebab3a2ffeb62473d23
vrajago2 <vrajago2@cisco.com>
2017-06-13 18:39:16 -0700

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+  render(
+    <div>
+      <h1>Hellow World!</h1>
+    </div>
+  );
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+export default const = combineReducers({
+  // fill in the modularized reducers here
+  // does not need to be a key-value pair object
+  /*
+    reducer1,
+    reducer 2,
+  */
+});
